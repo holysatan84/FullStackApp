026c8e1ea7fb77b8252744957df86216c7561523
Madhavi <madhavi.chavan@ness.com>
2016-05-30 17:41:16 +0530

    Adding changes to get userlist

--- Code/NodeApp/rechargeApp/app.js
+++ Code/NodeApp/rechargeApp/app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo =  require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/rechargeapp');
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,8 +27,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {	
+	console.log(db);
+	req.db = db;
+	next();
+});
+
+
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Code/NodeApp/rechargeApp/routes/index.js
+++ Code/NodeApp/rechargeApp/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET userlist page. */
+router.get('/userlist', function(req, res, next) {
+	var db = req.db;
+	var collection = db.get('users');
+	collection.find({},{"first_name":1,"pwd":0}, function(e, docs) {
+		res.render('userlist', { 'userlist' : docs });
+	});
+});
+
 module.exports = router;
