acca3afefd4d6e33b5b55dbfa98829ed8c6d759b
Sudeep Padalkar <sudeep.padalkar@ness.com>
2016-05-31 15:06:04 +0530

    added code to list users from datebase on http://localhost:3000/userlist

--- Code/NodeApp/rechargeApp/routes/users.js
+++ Code/NodeApp/rechargeApp/routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/userlist', function(req, res, next) {
+//  res.send('respond with a resource');
+    var db = req.db;
+    var collections = db.get('rechargeapp');
+    collections.find({}, {}, function (e, docs) {
+//        console.log(JSON.stringify(docs));
+        res.render('userlist', {
+            "userlist": docs
+        })
+    });
 });
 
 module.exports = router;
