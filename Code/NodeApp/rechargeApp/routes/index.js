37ea89ea0c786f73b1971d654809f959d0e3fddd
Sudeep Padalkar <sudeep.padalkar@ness.com>
2016-05-31 18:51:32 +0530

    functionality for adding user, userlist and user JSON

--- Code/NodeApp/rechargeApp/routes/index.js
+++ Code/NodeApp/rechargeApp/routes/index.js
@@ -2,17 +2,76 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 /* GET userlist page. */
-router.get('/userlist', function(req, res, next) {
-	var db = req.db;
-	var collection = db.get('users');
-	collection.find({},{"first_name":1,"pwd":0}, function(e, docs) {
-		res.render('userlist', { 'userlist' : docs });
-	});
+router.get('/userlist', function (req, res, next) {
+    var db = req.db;
+    var collection = db.get('users');
+    collection.find({}, {}, function (e, docs) {
+        res.render('userlist', {'userlist': docs});
+    });
+});
+
+/* GET user JSON. */
+router.get('/user/:id', function (req, res, next) {
+//    console.log('user ' + req.params.id);
+    var db = req.db;
+    var id = req.params.id;
+    var collection = db.get('users');
+    collection.findOne({"_id": id}, {}, function (e, doc) {
+        res.send(JSON.stringify(doc));
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function (req, res) {
+    res.render('newuser', {title: 'Add New User'});
+});
+
+router.post('/adduser', function (req, res) {
+    //Set the internal db variable
+    var db = req.db;
+
+    //Get the form values
+    var firstName = req.body.firstname;
+    var lastName = req.body.lastname;
+    var userEmail = req.body.useremail;
+    var phoneNumber = req.body.phonenumber;
+    var password = req.body.password;
+
+    //pre defined values
+    var userType = 1;
+    var status = 1;
+    var createDate = new Date();
+    var lastModified = null;
+    var lastLogin = null;
+
+    //Set our collection
+    var collection = db.get('users');
+
+    collection.insert({
+        "first_name": firstName,
+        "last_name": lastName,
+        "email_id": userEmail,
+        "phone_no": phoneNumber,
+        "user_type": userType,
+        "pwd": password,
+        "status": status,
+        "created_date": createDate,
+        "last_modified": lastModified,
+        "last_login": lastLogin
+    }, function (err, doc) {
+        if (err) {
+            res.send("A problem occured User could not be created");
+        } else {
+            res.redirect("/userlist");
+        }
+    });
+
+
 });
 
 module.exports = router;
--- Code/NodeApp/rechargeApp/routes/users.js
+++ Code/NodeApp/rechargeApp/routes/users.js
@@ -1,17 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/userlist', function(req, res, next) {
-//  res.send('respond with a resource');
-    var db = req.db;
-    var collections = db.get('rechargeapp');
-    collections.find({}, {}, function (e, docs) {
-//        console.log(JSON.stringify(docs));
-        res.render('userlist', {
-            "userlist": docs
-        })
-    });
-});
-
 module.exports = router;
